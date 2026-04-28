eb55c1c26f919a25d24feb129eaa3512b7c2ec11
David Medina <david.Medina@starken.cl>
2026-04-28 04:09:02 -0400

    Instalacion frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,11 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     ...(appConfig.providers || []),
-    provideAnimations() // 🔥 CLAVE
+    provideAnimations(), provideAnimationsAsync() // 🔥 CLAVE
   ]
 }).catch((err) => console.error(err));
