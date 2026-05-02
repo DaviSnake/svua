ac92c16e8e31d5d94e2cde2c4dfa0e4c903d5d65
David Medina <david.Medina@starken.cl>
2026-05-02 03:17:18 -0400

    Se agrega archivo

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Generando env.js..."
+
+cat <<EOF > /usr/share/nginx/html/assets/env.js
+window.__env = {
+  apiUrl: "${API_URL}"
+};
+EOF
+
+echo "env.js generado:"
+cat /usr/share/nginx/html/assets/env.js
+
+nginx -g "daemon off;"
