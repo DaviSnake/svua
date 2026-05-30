a05fa0cc0c0f14897506d216683de49100506326
David Medina <david.Medina@starken.cl>
2026-05-30 00:47:00 -0400

    Se crea tabla notificacion e indice a la tabla usuario

---
+++ svuaback/src/main/java/cl/aracridav/svua/notificacion/entity/TipoNotificacion.java
@@ -0,0 +1,13 @@
+package cl.aracridav.svua.notificacion.entity;
+
+public enum TipoNotificacion {
+
+    ORDEN_VENCIDA,
+    MANTENIMIENTO_HOY,
+    STOCK_BAJO,
+    NUEVA_ORDEN,
+    USUARIO_AGREGADO,
+    SUSCRIPCION_VENCE,
+    PAGO_CONFIRMADO,
+    ACTIVO_SIN_MANTENCION
+}
---
+++ svuaback/src/main/resources/db/V11__create_index_table_usuario.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_usuario_empresa_nombre
+ON usuario (empresa_id, nombre);
